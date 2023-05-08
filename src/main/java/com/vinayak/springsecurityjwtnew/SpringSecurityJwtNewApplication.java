3e6e7d13be9792b9da00a0a8c5598e262dd5da9e
Vinayak <vinayakchaurasia99@gmail.com>
2023-05-08 20:57:57 +0530

    Initial commit and DB properties

---
+++ src/main/java/com/vinayak/springsecurityjwtnew/SpringSecurityJwtNewApplication.java
@@ -0,0 +1,13 @@
+package com.vinayak.springsecurityjwtnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtNewApplication.class, args);
+	}
+
+}
