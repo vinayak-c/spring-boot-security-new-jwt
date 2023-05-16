f21cbe366ab2ec5f43a716b1a32a1f44b25556ec
Vinayak <vinayakchaurasia99@gmail.com>
2023-05-16 23:31:43 +0530

    Added configs

---
+++ src/main/java/com/vinayak/springsecurityjwtnew/config/JwtAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.vinayak.springsecurityjwtnew.config;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @Nonnull HttpServletRequest request,
+            @Nonnull HttpServletResponse response,
+            @Nonnull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+    }
+
+}
