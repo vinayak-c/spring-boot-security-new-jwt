f64b47ecea3a7ada050a199823e935c64c322abe
Vinayak Chaurasia <vinayakchaurasia@rathi.com>
2023-05-27 19:59:04 +0530

    Added auth service, controller and POJOs

---
+++ src/main/java/com/vinayak/springsecurityjwtnew/auth/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.vinayak.springsecurityjwtnew.auth;
+
+import com.vinayak.springsecurityjwtnew.config.JwtService;
+import com.vinayak.springsecurityjwtnew.entity.Role;
+import com.vinayak.springsecurityjwtnew.entity.User;
+import com.vinayak.springsecurityjwtnew.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public  AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow(() -> {throw new RuntimeException("User fetch error");});
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
