e53ff9461cad639dbcfc1d95b3fcfbc1de7d1ae2
Vinayak <vinayakchaurasia99@gmail.com>
2023-05-28 23:28:15 +0530

    Added user registration API as permit all permission

--- src/main/java/com/vinayak/springsecurityjwtnew/config/SecurityConfiguration.java
+++ src/main/java/com/vinayak/springsecurityjwtnew/config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration {
             .csrf()
             .disable()
             .authorizeHttpRequests()
-            .requestMatchers("")
+            .requestMatchers("/api/v1/auth/**")
             .permitAll()
             .anyRequest()
             .authenticated()
@@ -35,7 +35,7 @@ public class SecurityConfiguration {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return  http.build();
